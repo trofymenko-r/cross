cfae85b967b07b63cec0d866f4560cbc45908433
Ruslan Trofymenko <ruslan.trofymenko@linaro.org>
2019-10-22 12:32:18 +0300

    uexcept: specify namespaces of used functions
    Signed-off-by: Ruslan Trofymenko <ruslan.trofymenko@linaro.org>

--- inc/uexcept.h
+++ inc/uexcept.h
@@ -9,9 +9,10 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
 
-#define CODE_LOCATE  (string("file ") + __FILE__ + ", fuction " + __FUNCTION__ + ", line " + to_string(__LINE__))
-#define RT_ERROR(msg)  runtime_error(CODE_LOCATE + ": " + msg);
+#define CODE_LOCATE  (std::string("file ") + __FILE__ + ", fuction " + __FUNCTION__ + ", line " + std::to_string(__LINE__))
+#define RT_ERROR(msg)  std::runtime_error(CODE_LOCATE + ": " + msg);
 
 
 
